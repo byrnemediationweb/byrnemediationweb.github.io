187bd3aeee4a01d3724555adbdd6e1088bc4dc0c
Christopher <cgolle@2keys.ca>
2022-12-26 18:55:59 -0500

    tweaking form submission

--- contactform.js
+++ contactform.js
@@ -20,24 +20,36 @@
           event.preventDefault();
           event.stopPropagation();
         } else {
-          $("#submit-success").addClass("d-none");
-          $("#submit-error").addClass("d-none");
           event.preventDefault();
           event.stopPropagation();
           var formData = new FormData(form);
           var xhr = new XMLHttpRequest();
           xhr.open("POST", "https://usebasin.com/f/6ac25444f87c", true);
           xhr.send(formData);
+
+          let errorAlert = $('<div class="alert alert-danger alert-dismissable fade show d-flex align-items-center" role="alert">'
+          + '    <div>'
+          + '       Sorry, something went wrong and your message was not sent. Please email Donald Byrne directly: <a class="text-white" href="mailto:dbyrne@primus.ca">dbyrne@primus.ca</a>'
+          + '     </div>'
+          + '     <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>'
+          + '</div>');
+          let successAlert = $('<div class="alert alert-success alert-dismissable fade show d-flex align-items-center" role="alert">'
+          + '   <div>'
+          + 'Your message was submitted.'
+          + '    </div>'
+          + '    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>'
+          + '</div>');
+
           xhr.onload = function (e) {
             var response = JSON.parse(xhr.response);
             if (xhr.status === 200) {
               if (response.success) {
-                $("#submit-success").removeClass("d-none");
+                $(form).prepend(successAlert);
               } else {
-                $("#submit-error").removeClass("d-none");
+                $(form).prepend(errorAlert);
               }
             } else {
-              $("#submit-error").removeClass("d-none");
+              $(form).prepend(errorAlert);
             }
             form.reset();
           };
